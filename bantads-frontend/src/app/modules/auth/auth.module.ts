009263c795f051c4570b1eda64f69894549f1edf
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2024-03-11 19:24:01 -0300

    :bug: fix: corrigido modules

--- bantads-frontend/src/app/modules/auth/auth.module.ts
+++ bantads-frontend/src/app/modules/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from '../../pages/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { LoginComponent } from '../../pages/login/login.component';
     CommonModule,
     FormsModule,
     RouterModule,
+    HttpClientModule,
   ]
 })
 export class AuthModule { }
