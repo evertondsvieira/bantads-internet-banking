258e7193d3896e5358d195b5196d27440d8dc883
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2024-03-11 18:52:52 -0300

    :sparkles: feat: página home do cliente

--- bantads-frontend/src/app/pages/client/client-home/client-home.component.ts
+++ bantads-frontend/src/app/pages/client/client-home/client-home.component.ts
@@ -1,12 +1,75 @@
 import { Component } from '@angular/core';
 
+interface IFinancialBalance {
+  title: string
+  value: number
+}
+
+interface IOperation {
+  title: string;
+  link: string;
+}
+
+interface ITransaction {
+  title: string;
+  type: 'Receitas' | 'Despesas';
+  value: number;
+}
+
 @Component({
   selector: 'app-client-home',
-  standalone: true,
-  imports: [],
   templateUrl: './client-home.component.html',
   styleUrl: './client-home.component.css'
 })
 export class ClientHomeComponent {
+  operations: IOperation[] = [
+    { title: 'Depositar', icon: 'attach_money', link: '/deposit' },
+    { title: 'Sacar', icon: 'money_off', link: '/withdrawl' },
+    { title: 'Transferir', icon: 'compare_arrows', link: '/transfer' },
+    { title: 'Extrato', icon: 'description', link: '/statement' },
+  ]
 
+  balances: IFinancialBalance[] = [
+    { title: 'Receita', value: 50 },
+    { title: 'Despesas', value: 100 },
+    { title: 'Saldo Atual', value: -100 },
+  ]
+
+  transactions: ITransaction[] = [
+    { title: 'Salário', type: 'Receitas', value: 100 },
+    { title: 'Parcela do carro', type: 'Despesas', value: 100 },
+    { title: 'Parcela do carro', type: 'Despesas', value: 100 },
+  ]
+
+  filteredTransactions: ITransaction[] = [];
+
+  constructor() {
+    this.filteredTransactions = this.transactions;
+  }
+
+  filterRecipes(type: 'Receitas' | 'Despesas' | 'Todos') {
+    if (type === 'Todos') {
+      this.filteredTransactions = this.transactions;
+    } else {
+      this.filteredTransactions = this.transactions.filter(item => {
+        return item.type === type;
+      })
+    }
+  }
+
+  getBalanceColor(value: number) {
+    return value < 0 ? 'text-danger' : 'text-dark';
+  }
+
+  getTransactionColor(value: number, type: string) {
+    if (type === 'Despesas') {
+      return 'text-danger'
+    }
+  
+    if (value < 0) {
+      return 'text-danger'
+    }
+
+    return 'text-success'
+  }
 }
