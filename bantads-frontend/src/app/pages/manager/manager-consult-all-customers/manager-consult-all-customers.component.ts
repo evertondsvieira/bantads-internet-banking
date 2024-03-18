65280f4db1dd3bb04e6a36f60cc3d96d701d5a0c
Caroline Prockmann <carolprock@gmail.com>
2024-03-18 16:48:51 -0300

    R12-consultar todos os clientes

--- bantads-frontend/src/app/pages/manager/manager-consult-all-customers/manager-consult-all-customers.component.ts
+++ bantads-frontend/src/app/pages/manager/manager-consult-all-customers/manager-consult-all-customers.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Client } from '../../../models/client.model';
+import { ClientService } from '../../../services/client/client.service';
+import { User } from '../../../models/user.model';
+import { AuthService } from '../../../services/auth/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-manager-consult-all-customers',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './manager-consult-all-customers.component.html',
-  styleUrl: './manager-consult-all-customers.component.css'
+  styleUrl: './manager-consult-all-customers.component.css',
 })
-export class ManagerConsultAllCustomersComponent {
+export class ManagerConsultAllCustomersComponent implements OnInit {
+  private _user: User = this.authService.loggedUser;
+  private _clients!: Client[];
+  public searchQuery: string = '';
 
+  constructor(
+    private authService: AuthService,
+    private clientService: ClientService
+  ) {}
+
+  ngOnInit(): void {
+    this._user = this.authService.loggedUser;
+    this.getClientsOrderedByName();
+  }
+
+  public get clients(): Client[] {
+    return this._clients;
+  }
+  public set clients(value: Client[]) {
+    this._clients = value;
+  }
+
+  public getUser(): User {
+    return this._user;
+  }
+  public set user(user: User) {
+    this._user = user;
+  }
+
+  public getClientsOrderedByName() {
+    this.clientService.getClientsOrderedByName().subscribe((clients) => {
+      this._clients = clients;
+    });
+  }
+
+  public searchClients() {
+    if (this.searchQuery.trim() !== '') {
+      this.clientService
+        .searchClients(this.searchQuery)
+        .subscribe((clients) => {
+          this._clients = clients;
+        });
+    } else {
+      this.getClientsOrderedByName();
+    }
+  }
 }
