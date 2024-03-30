6d93dd4bfd05eecc343acd5ceb6aba66dec344a1
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2024-03-29 22:45:45 -0300

    :sparkles: feat: criado ms cliente

---
+++ bantads-backend/ms-client/src/test/java/bantads/msclient/MsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package bantads.msclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
