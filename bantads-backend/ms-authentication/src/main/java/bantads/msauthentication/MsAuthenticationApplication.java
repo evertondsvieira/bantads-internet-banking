6d34ab9de2547c45558150ba06545a358f69e0dd
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2024-03-29 22:42:17 -0300

    :sparkles: feat: criado ms autenticação

---
+++ bantads-backend/ms-authentication/src/main/java/bantads/msauthentication/MsAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package bantads.msauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAuthenticationApplication.class, args);
+	}
+
+}
