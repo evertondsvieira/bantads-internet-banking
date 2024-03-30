88896622eba301e4721faa8cc989df8eeb75c51b
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2024-03-29 22:46:51 -0300

    :sparkles: feat: criado ms gerente

---
+++ bantads-backend/ms-manager/src/test/java/bantads/msmanager/MsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package bantads.msmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
