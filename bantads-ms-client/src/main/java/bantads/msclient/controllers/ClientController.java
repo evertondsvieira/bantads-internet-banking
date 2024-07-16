ea0ac9c46445c4416c52e6a8e1d12ea3ffdf70fd
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2024-07-15 21:49:34 -0300

    :sparkles: feat: verificando existencia do endereço

--- bantads-ms-client/src/main/java/bantads/msclient/controllers/ClientController.java
+++ bantads-ms-client/src/main/java/bantads/msclient/controllers/ClientController.java
@@ -1,9 +1,14 @@
 package bantads.msclient.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import bantads.msclient.dto.AddressDTO;
 import bantads.msclient.dto.ClientDTO;
 import bantads.msclient.dto.LoginRequestDTO;
 import bantads.msclient.dto.LoginResponseDTO;
 import bantads.msclient.entity.Client;
+import bantads.msclient.entity.Address;
 import bantads.msclient.exception.ClientAlreadyExistsException;
 import bantads.msclient.exception.InvalidCredentialsException;
 import bantads.msclient.service.ClientService;
@@ -19,6 +24,8 @@ import jakarta.validation.Valid;
 @RequestMapping("/api")
 public class ClientController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ClientController.class);
+
     @Autowired
     private ClientService clientService;
 
@@ -28,11 +35,34 @@ public class ClientController {
     @PostMapping("/client")
     public ResponseEntity<Client> registerClient(@Valid @RequestBody ClientDTO clientDTO) {
         try {
-            Client newClient = clientService.createClient(clientDTO);
-            return new ResponseEntity<>(newClient, HttpStatus.CREATED);
+            Client newClient = new Client();
+            newClient.setName(clientDTO.getName());
+            newClient.setEmail(clientDTO.getEmail());
+            newClient.setCpf(clientDTO.getCpf());
+            newClient.setPhone(clientDTO.getPhone());
+            newClient.setSalary(clientDTO.getSalary());
+            newClient.setPassword(clientDTO.getPassword());
+
+            Address address = new Address();
+            AddressDTO addressDTO = clientDTO.getAddress();
+            if (addressDTO != null) {
+                address.setType(addressDTO.getType());
+                address.setStreet(addressDTO.getStreet());
+                address.setNumber(addressDTO.getNumber());
+                address.setComplement(addressDTO.getComplement());
+                address.setCep(addressDTO.getCep());
+                address.setCity(addressDTO.getCity());
+                address.setState(addressDTO.getState());
+            }
+            newClient.setAddress(address);
+
+            Client createdClient = clientService.createClient(newClient);
+            return new ResponseEntity<>(createdClient, HttpStatus.CREATED);
         } catch (ClientAlreadyExistsException e) {
+            logger.error("Client already exists: {}", e.getMessage());
             return new ResponseEntity<>(null, HttpStatus.CONFLICT);
         } catch (Exception e) {
+            logger.error("Error registering client: {}", e.getMessage());
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
     }
