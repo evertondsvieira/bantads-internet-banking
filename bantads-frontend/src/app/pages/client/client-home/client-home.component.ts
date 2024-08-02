517559fe2619679b1653612b5f885ea2736890fd
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2024-08-02 19:49:58 -0300

    :recycle: refactor: refatorando página home do cliente

--- bantads-frontend/src/app/pages/client/client-home/client-home.component.ts
+++ bantads-frontend/src/app/pages/client/client-home/client-home.component.ts
@@ -1,9 +1,7 @@
-import { Component } from '@angular/core';
-
-interface IFinancialBalance {
-  title: string
-  value: number
-}
+import { Component, OnInit } from '@angular/core';
+import { Account } from '../../../models/account.model';
+import { AccountService } from '../../../services/account/account.service';
+import { ActivatedRoute } from '@angular/router';
 
 interface IOperation {
   title: string;
@@ -11,66 +9,59 @@ interface IOperation {
   link: string;
 }
 
-interface ITransaction {
-  title: string;
-  type: 'Receitas' | 'Despesas';
-  value: number;
-}
-
 @Component({
   selector: 'app-client-home',
   templateUrl: './client-home.component.html',
   styleUrl: './client-home.component.css'
 })
-export class ClientHomeComponent {
+export class ClientHomeComponent implements OnInit {
+  accountData!: Account
+  saldo: number = -20
+
+  userId = localStorage.getItem('userId')
+
   operations: IOperation[] = [
-    { title: 'Depositar', icon: 'attach_money', link: '/deposit' },
-    { title: 'Sacar', icon: 'money_off', link: '/withdrawl' },
-    { title: 'Transferir', icon: 'compare_arrows', link: '/transfer' },
-    { title: 'Extrato', icon: 'description', link: '/statement' },
+    { title: 'Depositar', icon: 'attach_money', link: `deposit/${this.userId}` },
+    { title: 'Sacar', icon: 'money_off', link: `withdrawl/${this.userId}` },
+    { title: 'Transferir', icon: 'compare_arrows', link: `transfer/${this.userId}` },
+    { title: 'Extrato', icon: 'description', link: 'statement' },
   ]
 
-  balances: IFinancialBalance[] = [
-    { title: 'Receita', value: 50 },
-    { title: 'Despesas', value: 100 },
-    { title: 'Saldo Atual', value: -100 },
-  ]
-
-  transactions: ITransaction[] = [
-    { title: 'Salário', type: 'Receitas', value: 100 },
-    { title: 'Parcela do carro', type: 'Despesas', value: 100 },
-    { title: 'Parcela do carro', type: 'Despesas', value: 100 },
-  ]
-
-  filteredTransactions: ITransaction[] = [];
-
-  constructor() {
-    this.filteredTransactions = this.transactions;
+  initializeOperations(): void {
+    if (this.userId) {
+      this.operations = [
+        { title: 'Depositar', icon: 'attach_money', link: `deposit/${this.userId}` },
+        { title: 'Sacar', icon: 'money_off', link: `withdrawl/${this.userId}` },
+        { title: 'Transferir', icon: 'compare_arrows', link: `transfer/${this.userId}` },
+        { title: 'Extrato', icon: 'description', link: 'statement' },
+      ];
+    }
   }
 
-  filterRecipes(type: 'Receitas' | 'Despesas' | 'Todos') {
-    if (type === 'Todos') {
-      this.filteredTransactions = this.transactions;
-    } else {
-      this.filteredTransactions = this.transactions.filter(item => {
-        return item.type === type;
+  constructor(
+    private accountService: AccountService,
+  ) {}
+
+  ngOnInit(): void {
+    this.getAccount()
+  }
+
+  getAccount(): void {
+    if (this.userId) {
+      this.accountService.getAccountById(Number(this.userId)).subscribe({
+        next: (account: Account) => {
+          this.accountData = account
+        },
+        error: (error) => {
+          console.error('Erro ao carregar cliente', error)
+        }
       })
+    } else {
+      console.error('ID do cliente não fornecido')
     }
   }
 
   getBalanceColor(value: number) {
     return value < 0 ? 'text-danger' : 'text-dark';
-  }
-
-  getTransactionColor(value: number, type: string) {
-    if (type === 'Despesas') {
-      return 'text-danger'
-    }
-  
-    if (value < 0) {
-      return 'text-danger'
-    }
-
-    return 'text-success'
   }
 }
