e460bc2859d15a71952fb6c76976759d2b5fb553
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2024-08-02 18:37:49 -0300

    :sparkles: feat: requisição get e put para o deposito

--- bantads-frontend/src/app/pages/client/client-deposit/client-deposit.component.ts
+++ bantads-frontend/src/app/pages/client/client-deposit/client-deposit.component.ts
@@ -1,45 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Account } from '../../../models/account.model';
 import { AccountService } from '../../../services/account/account.service';
 import { Transaction } from '../../../models/transaction.model';
-import { User } from '../../../models/user.model';
-import { AuthService } from '../../../services/auth/auth.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ModalTransactionComponent } from '../../../components/modal/modal-transaction/modal-transaction.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-client-deposit',
   templateUrl: './client-deposit.component.html',
-  styleUrl: './client-deposit.component.css'
+  styleUrls: ['./client-deposit.component.css']
 })
-export class ClientDepositComponent {
+export class ClientDepositComponent implements OnInit {
   accountData!: Account;
-  transaction: Transaction = new Transaction(new Date(), "Deposit");
-  user!: User
-  
-  constructor (
+  transaction: Transaction = new Transaction("DEPOSIT");
+  userId: number = 0;
+
+  constructor(
     private accountService: AccountService,
     private modalService: NgbModal,
-    private authService: AuthService
-  ){}
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.getAccount();
-    this.user = this.authService.loggedUser;
-  }
+    this.route.paramMap.subscribe(params => {
+      const idParam = params.get("id")
 
-  getAccount() {
-    this.accountService.getAccountByCPF(/*this.user.cpf*/'11111111111').subscribe(
-      (accountsData: Account[]) => {
-        this.accountData = accountsData[0];
-        this.transaction.originAccount = accountsData[0].accountId;        
+      if (idParam && !isNaN(+idParam)) {
+        this.userId = +idParam
+        this.loadAccount()
+      } else {
+        console.log("Id do cliente é inválido")
       }
-    )
+    })
   }
 
-  openModal(transaction: Transaction){
+  loadAccount(): void {
+    this.getAccount();
+  }
+
+  getAccount(): void {
+    if (this.userId) {
+      this.accountService.getAccountById(this.userId).subscribe({
+        next: (account: Account) => {
+          this.accountData = account
+          this.transaction = new Transaction("DEPOSIT", undefined, this.accountData.id, this.accountData.id)
+        },
+        error: (error) => {
+          console.error('Erro ao carregar cliente', error)
+        }
+      })
+    } else {
+      console.error('ID do cliente não fornecido')
+    }
+  }
+
+  openModal(transaction: Transaction): void {
     const modalRef = this.modalService.open(ModalTransactionComponent);
     modalRef.componentInstance.transaction = this.transaction;
   }
-
 }
