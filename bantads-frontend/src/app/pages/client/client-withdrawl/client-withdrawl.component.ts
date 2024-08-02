557da4f53439cf770421ed9ab755025520933b8c
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2024-08-02 18:39:02 -0300

    :sparkles: feat: requisição get e put para o saque

--- bantads-frontend/src/app/pages/client/client-withdrawl/client-withdrawl.component.ts
+++ bantads-frontend/src/app/pages/client/client-withdrawl/client-withdrawl.component.ts
@@ -1,40 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Account } from '../../../models/account.model';
 import { AccountService } from '../../../services/account/account.service';
 import { Transaction } from '../../../models/transaction.model';
-import { User } from '../../../models/user.model';
-import { AuthService } from '../../../services/auth/auth.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ModalTransactionComponent } from '../../../components/modal/modal-transaction/modal-transaction.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-client-withdrawl',
   templateUrl: './client-withdrawl.component.html',
   styleUrl: './client-withdrawl.component.css'
 })
-export class ClientWithdrawlComponent {
+export class ClientWithdrawlComponent implements OnInit {
   accountData!: Account;
-  transaction: Transaction = new Transaction(new Date(), "Withdrawl");
-  user!: User
+  transaction: Transaction = new Transaction("WITHDRAWL");
+  userId: number = 0;
   
   constructor (
     private accountService: AccountService,
     private modalService: NgbModal,
-    private authService: AuthService
+    private route: ActivatedRoute
   ){}
 
   ngOnInit(): void {
-    this.getAccount();
-    this.user = this.authService.loggedUser;
+    this.route.paramMap.subscribe(params => {
+      const idParam = params.get("id")
+
+      if (idParam && !isNaN(+idParam)) {
+        this.userId = +idParam
+        this.loadAccount()
+      } else {
+        console.log("Id do cliente é inválido")
+      }
+    })
   }
 
-  getAccount() {
-    this.accountService.getAccountByCPF(/*this.user.cpf*/'11111111111').subscribe(
-      (accountsData: Account[]) => {
-        this.accountData = accountsData[0];
-        this.transaction.originAccount = accountsData[0].accountId;
-      }
-    )
+  loadAccount(): void {
+    this.getAccount();
+  }
+
+  getAccount(): void {
+    if (this.userId) {
+      this.accountService.getAccountById(this.userId).subscribe({
+        next: (account: Account) => {
+          this.accountData = account
+          this.transaction = new Transaction("WITHDRAWL", undefined, this.accountData.id, this.accountData.id)
+        },
+        error: (error) => {
+          console.error('Erro ao carregar cliente', error)
+        }
+      })
+    } else {
+      console.error('ID do cliente não fornecido')
+    }
   }
 
   openModal(transaction: Transaction){
